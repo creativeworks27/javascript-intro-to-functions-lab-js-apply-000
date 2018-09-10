3adc70e5a1b29163025669bf6a8eb6fbc5e4dd10
Roberto Romero <creativeworks27@icloud.com>
2018-09-10 02:05:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,13 +2,17 @@ function shout(string)
 {
 return string.toUpperCase();
 }
+
+
 function whisper(string) 
 {
 return string.toLowerCase();
 }
+
+var logShout = 
 function logShout(string) 
 {
 console.log.string.toUpperCase();
  
-console.log.restore()
+console.log.restore();
 }
