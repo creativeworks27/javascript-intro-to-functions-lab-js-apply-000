5b87f04a4a14fe84d2d6d0a11de74209df7d447a
Roberto Romero <creativeworks27@icloud.com>
2018-09-10 04:11:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,3 +19,11 @@ function logWhisper(string)
 var FGH = string.toLowerCase();
 console.log(FGH); 
 }
+function sayHiToGrandma(string) {
+  if (string.toLowerCase() === string) 
+  return "I can't hear you!";
+  if (string.toUpperCase() === string) 
+  return "YES INDEED!";
+  if (string==="I love you, Grandma.") 
+  return "I love you, too."
+}
