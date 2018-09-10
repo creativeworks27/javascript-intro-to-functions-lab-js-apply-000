af3a360f48e2d1f923be426886a435f0718ac02f
Roberto Romero <creativeworks27@icloud.com>
2018-09-10 04:07:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,11 +24,11 @@ function sayHiToGrandma(string)
 {
   if (string === string.toLowerCase())
 
-  return "I can't hear you!";
+return "I can't hear you!";
   }
-  if (string === "I love you, Grandma.") {
-  return "I love you, too.";
+if (string === "I love you, Grandma.") {
+return "I love you, too.";
   }
-  if (string === string.toUpperCase()) {
-  return "YES INDEED!";
+if (string === string.toUpperCase()) {
+return "YES INDEED!";
   }
