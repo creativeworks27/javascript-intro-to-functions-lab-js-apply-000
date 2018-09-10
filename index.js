fff0c9bdc48f853991337a4e60c3901ae32fda66
Roberto Romero <creativeworks27@icloud.com>
2018-09-10 02:11:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,10 +9,9 @@ function whisper(string)
 return string.toLowerCase();
 }
 
-var sayHiToGrandma = "i love you grandma.";
-function logShout(string) 
+function logShout(string)
+
 {
 console.log.string.toUpperCase();
  
-console.log.restore();
 }
